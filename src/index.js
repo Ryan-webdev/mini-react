07b5b9e952526fec9a83d45c83585bc568cc98c7
Ryan Schueck <ryschueck@gmail.com>
2022-04-26 19:23:21 -0400

    created create element function

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import "./styles.css";
 
 const container = document.getElementById("app");
 
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "hello"
-  }
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+const Didact = {
+  createElement
 };
-
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-
-node.appendChild(text);
-
-container.appendChild(node);
+/**@jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
